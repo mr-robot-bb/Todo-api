038d5b9161666814a85546dbf069f8358aad609d
Henderikus Top <henderikus.top@gmail.com>
2016-01-03 18:43:16 +0100

    refactor with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -20,15 +21,8 @@ app.get('/todos', function(req, res){
 // GET /todos/:id
 app.get('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
-	
-	todos.forEach(function(todo){
-		if(todo.id === todoId){
-			console.log('found match');
-			matchedTodo = todo;
-		}
-	});
-	
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+		
 	if(matchedTodo){
 		res.json(matchedTodo);
 	}else{
@@ -39,7 +33,14 @@ app.get('/todos/:id', function(req, res){
 
 // POST /todos
 app.post('/todos', function(req, res){
-	var body = req.body;
+	var body = _.pick(req.body, 'description','completed');
+	
+	
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ){
+		return res.status(400).send();
+	}
+	body.description = body.description.trim();
+	
 	body.id = todoNextId;
 	todoNextId++;
 	todos.push(body);
