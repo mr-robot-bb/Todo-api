f646afc022a7cce24fb61eac44d2c400a0d56d34
Henderikus Top <henderikus.top@gmail.com>
2016-01-06 21:19:43 +0100

    Add variable playground

---
+++ variable-playground.js
@@ -0,0 +1,34 @@
+/*
+var person = {
+	name: 'Andrew',
+	age: 21
+}
+
+function updatePerson(obj){
+	
+	//obj = {
+	//	name: 'Andrew',
+	//	age: 34
+	//}
+	
+	
+	obj.age = 34;
+}
+
+updatePerson(person);
+console.log(person);
+*/
+
+// array example
+
+var fruits = ["banana","apple"];
+
+function updateArray(arr){
+	//arr.push("kiwi");
+	arr = ["banana","apple","kiwi"];
+	debugger;
+}
+
+updateArray(fruits);
+
+console.log(fruits);
