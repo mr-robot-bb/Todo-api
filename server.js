07dfac919a7b82db098b6f03b1baa387f0940dff
Henderikus Top <henderikus.top@gmail.com>
2016-01-14 19:11:42 +0100

    Add auth token generation to POST/users/login

--- server.js
+++ server.js
@@ -221,8 +221,14 @@ app.post('/users', function(req, res){
 app.post('/users/login', function(req, res){
 	var body = _.pick(req.body, 'email', 'password');
 	db.user.authenticate(body).then(function(user){
-		console.log('User found')
-		res.json(user.toPublicJSON());
+		console.log('User found');
+		var token = user.generateToken('authentication')
+		if(token){
+			res.header('Auth', token).json(user.toPublicJSON());
+		}else{
+			res.status(401).send(); // no token...
+		}
+		
 	}, function() {
 		console.log('Something wrong');
 		res.status(401).send();
