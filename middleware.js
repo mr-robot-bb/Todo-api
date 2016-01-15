b63c18468422f3eddca14761784f57d0818646f7
Henderikus Top <henderikus.top@gmail.com>
2016-01-15 20:55:18 +0100

    Added DELETE users/login

--- middleware.js
+++ middleware.js
@@ -1,16 +1,37 @@
+var cryptojs = require('crypto-js');
+
 module.exports = function (db) {
 
 	return {
 		requireAuthentication: function(req, res, next){
-			var token = req.get('Auth');
+			var token = req.get('Auth') || '';
 			console.log('checking user');
-			db.user.findByToken(token).then(function(user){
+			
+			db.token.findOne({
+				where: {
+					tokenHash: cryptojs.MD5(token).toString()
+				}
+			}).then(function(tokenInstance){
+				if(!tokenInstance){
+					throw new Error();
+				}
+				req.token = tokenInstance;
+				return db.user.findByToken(token);
+			}).then(function(user){
 				req.user = user;
 				next();
-			}, function(){
-				console.log('user not authenticated');
+			}).catch(function(){
 				res.status(401).send();
 			});
+
+			// db.user.findByToken(token).then(function(user){
+			// 	req.user = user;
+			// 	next();
+			// }, function(){
+			// 	console.log('user not authenticated');
+			// 	res.status(401).send();
+			// });
+
 		}
 	};
 
